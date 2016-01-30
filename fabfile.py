be55f40e56127e10779c0ec6ee2d628dbcf529ce
zkan <kan@prontomarketing.com>
2016-01-30 20:26:17 +0700

    Use fabric to deploy Puppet modules

---
+++ fabfile.py
@@ -0,0 +1,32 @@
+from fabric.api import cd, env, local, lcd, run
+
+
+PUPPET_MASTER_IP = '192.168.33.10'
+
+
+def puppet():
+    env.hosts = [
+        'vagrant@' + PUPPET_MASTER_IP + ':22',
+    ]
+    env.passwords = {
+        'vagrant@' + PUPPET_MASTER_IP + ':22': 'vagrant'
+    }
+
+
+def test():
+    with lcd('puppet/modules'):
+        with lcd('nginx'):
+            local('rspec')
+
+
+def deploy():
+    puppet()
+    test()
+
+    run('rm -rf puppet-untitled-2016')
+    run('git clone https://github.com/zkan/puppet-untitled-2016.git')
+
+    run('sudo rm -rf /etc/puppet/manifests')
+    run('sudo ln -sf /home/vagrant/puppet-untitled-2016/puppet/manifests /etc/puppet/manifests')
+    run('sudo rm -rf /etc/puppet/modules')
+    run('sudo ln -sf /home/vagrant/puppet-untitled-2016/puppet/modules /etc/puppet/modules')
