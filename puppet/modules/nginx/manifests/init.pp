2a724972416f622c19a26f5afae7cc46ce9cc48d
zkan <kan@prontomarketing.com>
2016-02-03 07:39:37 +0700

    Use template and param to define server name for nginx configuration

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,5 +1,12 @@
-class nginx {
+class nginx ($server_name = "puppet-workshop") {
   package { "nginx":
     ensure => "installed"
+  }
+
+  file { "/etc/nginx/sites-available/default":
+    content => template("nginx/default.erb"),
+    owner => "root",
+    group => "root",
+    require => Package["nginx"]
   }
 }
--- puppet/modules/nginx/spec/classes/nginx_spec.rb
+++ puppet/modules/nginx/spec/classes/nginx_spec.rb
@@ -9,4 +9,32 @@ describe "nginx" do
             "ensure"  => "installed"
         )
     }
+
+    it {
+      should contain_file("/etc/nginx/sites-available/default").with_content(
+        /server_name puppet-workshop;/
+      ).with(
+        "owner" => "root",
+        "group" => "root",
+        "require" => "Package[nginx]"
+      )
+    }
+
+    context "with specific server name" do
+      let(:params) {
+        {
+          :server_name => "untitled-2016"
+        }
+      }
+
+      it {
+        should contain_file("/etc/nginx/sites-available/default").with_content(
+          /server_name untitled-2016;/
+        ).with(
+          "owner" => "root",
+          "group" => "root",
+          "require" => "Package[nginx]"
+        )
+      }
+    end
 end
